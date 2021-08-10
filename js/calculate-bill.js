37ec740d058b7cf6c83c4de8e7aa1aee7ea35e11
Pretty <mphefopretty1997@gmail.com>
2021-08-10 21:15:06 +0200

    first commit

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -11,3 +11,28 @@
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
 //link the function to a click event on the calculate button
+
+const billStringElement = document.querySelector(".billString");
+// do this for all the DOM elements
+function calculateBtnClicked(){
+    // get the string entered in the textArea
+    var billString = billStringElement.value;
+    //split the string
+    var billItems = billString.split(",");
+    // a variable for the total phone bill.
+    var billTotal = 0;
+    //loop over all the bill items
+    for (var i=0;i<billItems.length;i++){
+        var billItem = billItems[i].trim();
+        if (billItem === "call"){
+            billTotal += 2.75;
+        }
+        else if (billItem === "sms"){
+            billTotal += 0.75;
+        }
+    }
+    
+    //round to two decimals
+    var roundedBillTotal = billTotal.toFixed(2);
+    billTotalElement.innerHTML = roundedBillTotal;
+}
